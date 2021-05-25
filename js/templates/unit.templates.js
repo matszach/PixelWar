a25f1dbd88fee91a90fb7bb2b1985773a00e37bf
matszach <lkaszubowski@gmail.com>
2021-05-26 00:27:27 +0200

    axeman

--- js/templates/unit.templates.js
+++ js/templates/unit.templates.js
@@ -4,6 +4,7 @@ const UNIT_TEMPLATES = new Repository(
     ['P_PEASANT', 'Peasant', 'player-units', 0, 0, 18, 20, 0, 10, 2, 10, 50, 120, [20, 5, 30]],
     ['P_SWORDSMAN', 'Swordsman', 'player-units', 0, 1, 22, 30, 2, 15, 1.5, 30, 60, 140, [20, 5, 40]],
     ['P_WIZARD', 'Wizard', 'player-units', 0, 2, 18, 15, 0, 30, 1, 40, 400, 240, [30, 20, 30]],
+    ['P_AXEMAN', 'Axeman', 'player-units', 0, 3, 24, 40, 0, 25, 2.5, 80, 70, 180, [40, 5, 40]],
     // ENEMY UNITS
     ['E_GRUNT', 'Grunt', 'enemy-units', 0, 0, 18, 15, 0, 8, 2, 8, 50, 180, [20, 5, 30]],
     ['E_SKELETON', 'Skeleton', 'enemy-units', 0, 1, 16, 10, 0, 6, 0.5, 5, 50, 210, [30, 5, 40]],
